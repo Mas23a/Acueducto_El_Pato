928f22e427e044f208cd0180cda4dc29373cb0dc
Mas23a <angelmigue23@hotmail.com>
2023-07-21 14:13:22 -0500

    Ajustes Página

--- acueductoPato/src/app/app.module.ts
+++ acueductoPato/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
+
+const routes: Routes=[
+  {path: '', redirectTo:'/principal', pathMatch:'full'},
+  {path: 'principal', component:PrincipalComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
